d2ecf451dc59fb1d203eee44619f0571539485f8
sibelly <sibellychan@hotmail.com>
2023-03-01 22:36:58 -0400

    feat: adds camera

---
+++ pages/video-player/src/controller.js
@@ -0,0 +1,15 @@
+export default class Controller {
+  constructor({ }) {
+
+  }
+
+  static async initialize(deps) {
+    const controller = new Controller(deps)
+    return controller.init()
+  }
+
+  async init() {
+    console.log("initi!!")
+  }
+
+}
